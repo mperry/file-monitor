ee41d429a4b005be8c995ec9da5cbef62a1b8879
Mark Perry <maperry78@yahoo.com.au>
2014-08-18 09:16:09 +1000

    Cleanup test code

--- src/test/java/com/github/mperry/Test3.java
+++ src/test/java/com/github/mperry/Test3.java
@@ -2,8 +2,10 @@ package com.github.mperry;
 
 import com.github.mperry.watch.Rx;
 import com.github.mperry.watch.Util;
+import fj.F;
 import fj.P1;
 import fj.P2;
+import fj.Unit;
 import fj.data.Option;
 import fj.data.Stream;
 import org.apache.commons.io.FileUtils;
@@ -20,6 +22,8 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 
+import static fj.Unit.unit;
+
 
 /**
  * Created by mperry on 12/08/2014.
@@ -30,91 +34,86 @@ public class Test3 {
 
     void println(Object o) {
 		log.info(o.toString());
-//        System.out.println(o);
     }
 
     @Test
-    public void test1() {
-        println("Thread: " + Thread.currentThread().getId());
+    public void testStreamIntegers() {
+        Util.printThread();
         Observable<Integer> o = Observable.from(Stream.range(1));
-        println("subscribing...");
         Observable<Integer> o2 = o.subscribeOn(Schedulers.computation());
         Subscription s = o2.subscribe(i -> {
             println(String.format("thread: %s, num: " + i, Thread.currentThread().getId()));
         });
         println("subscribed.");
-//        s.unsubscribe();
         println("sleeping...");
         sleep(100);
         s.unsubscribe();
         println("done");
-
     }
 
     public void sleep(int n) {
         try {
             Thread.sleep(n);
         } catch (InterruptedException e) {
-//            e.printStackTrace();
             log.error(e.getMessage(), e);
         }
-//        println("done");
     }
 
-	// WARNING: does not end without input from user
+    public <A> void runObservable(Observable<A> obs, F<A, Unit> print) {
+        log.info("set subscribe on...");
+        Observable<A> o2  = obs.subscribeOn(Schedulers.io());
+        log.info("subscribing...");
+        Subscription s = o2.subscribe((A a) -> {
+            print.f(a);
+        });
+        log.info("subscribed");
+        log.info("sleeping...");
+        sleep(2000);
+        log.info("done sleeping");
+        s.unsubscribe();
+        log.info("unsubscribed");
+    }
+
     @Test
-    public void test2() {
+    public void testObservableInactive() {
         try {
             log.info("create observable...");
 			P2<WatchService, WatchKey> p = Rx.register(Rx.DEFAULT_DIR, Util.ALL_EVENTS);
 			P1<Observable<WatchEvent<Path>>> o1 = Rx.observableInactive(p._1(), p._2());
-            log.info("set subscribe on...");
-            Observable<WatchEvent<Path>> o2  = o1._1().subscribeOn(Schedulers.io());
-            log.info("subscribing...");
-            Subscription s = o2.subscribe(we -> {
-                println(String.format("kind: %s, context: %s", we.kind(), we.context()));
-            });
-            log.info("subscribed");
-            log.info("sleeping...");
-            Thread.sleep(5000);
-            log.info("done sleeping");
-            s.unsubscribe();
-            log.info("unsubscribed");
-//			Thread.sleep(10000);
+            runObservable(o1._1(), printWatchEvent());
         } catch (IOException e) {
 			log.error(e.getMessage(), e);
-//            e.printStackTrace();
-        } catch (InterruptedException e) {
-            log.error(e.getMessage(), e);
         }
-
     }
 
-
-
     @Test
-    public void unsubscribeObservableOption() {
+    public void testObservableOptions() {
         try {
             log.info("create observable...");
 			P2<WatchService, WatchKey> p = Rx.register(Rx.DEFAULT_DIR, Util.ALL_EVENTS);
 			P1<Observable<Option<WatchEvent<Path>>>> o1 = Rx.observableOptions(p._1(), p._2());
-            log.info("set subscribe on...");
-            Observable<Option<WatchEvent<Path>>> o2  = o1._1().subscribeOn(Schedulers.io());
-            log.info("subscribing...");
-            Subscription s = o2.subscribe(option -> printOWE(option));
-            log.info("subscribed");
-            log.info("sleeping...");
-            sleep(5000);
-            log.info("done sleeping");
-            s.unsubscribe();
-            log.info("unsubscribed");
+            runObservable(o1._1(), printOptionWatchEvent());
         } catch (IOException e) {
 			log.error(e.getMessage(), e);
-//            e.printStackTrace();
-//        } catch (InterruptedException e) {
-//            log.error(e.getMessage(), e);
         }
+    }
 
+    F<WatchEvent<Path>, Unit> printWatchEvent() {
+        return we -> {
+            printWatchEvent(we);
+            return unit();
+        };
+    }
+
+    F<Option<WatchEvent<Path>>, Unit> printOptionWatchEvent() {
+        return o -> {
+            printOWE(o);
+            return unit();
+        };
+    }
+
+    void printWatchEvent(WatchEvent<Path> we) {
+        println(String.format("thread: %d, kind: %s, context: %s", Util.threadId(), we.kind(), we.context()));
     }
 
 	void printOWE(Option<WatchEvent<Path>> option) {
@@ -122,43 +121,14 @@ public class Test3 {
 			println("Option is none");
 		}
 		option.map(we -> {
-			println(String.format("thread: %d, kind: %s, context: %s", Util.threadId(), we.kind(), we.context()));
-			return we;
+            printWatchEvent(we);
+            return we;
 		});
-	}
-
-	@Test
-	public void takeObservableOption() {
-		try {
-			log.info("create observable...");
-			P2<WatchService, WatchKey> p = Rx.register(Rx.DEFAULT_DIR, Util.ALL_EVENTS);
-			P1<Observable<Option<WatchEvent<Path>>>> po = Rx.observableOptions(p._1(), p._2());
-			log.info("set subscribe on...");
-			Observable<Option<WatchEvent<Path>>> obs = po._1().subscribeOn(Schedulers.io());
-//			Observable<Option<WatchEvent<Path>>> obs = po._1();
-			log.info("subscribing...");
-			Subscription s = obs.subscribe(option -> printOWE(option));
-			log.info("subscribed");
-			generateEvents(10);
-			Observable<Option<WatchEvent<Path>>> obs2 = obs.take(3);
-			println("about to print count...");
-			obs2.count().take(1).forEach(i-> println("count2: " + i));
-			sleep(2000);
-			log.info("sleeping...");
-//			sleep(8000);
-			log.info("done sleeping");
-			s.unsubscribe();
-			log.info("unsubscribed");
-		} catch (IOException e) {
-			log.error(e.getMessage(), e);
-		}
-
 	}
 
 	@Test
 	public void testGenerateEvents() {
 		generateEvents(3);
-
 	}
 
 	void generateEventsAsync(int n) {
@@ -184,12 +154,7 @@ public class Test3 {
 
 	void append(File f, String text) throws IOException {
 		FileUtils.writeStringToFile(f, text, true);
-
 	}
 
-	@Test
-	public void testSimple() {
-		log.info("Logging test simple");
-	}
 
 }
